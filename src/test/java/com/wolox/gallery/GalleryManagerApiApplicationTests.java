226efcb4e2db86120429f372a1ef1165b90dc202
pacheco <maguilar@esecartagenadeindias.gov.co>
2020-12-28 17:08:11 -0500

    configuracion del proyecto y dependencias necesarias

---
+++ src/test/java/com/wolox/gallery/GalleryManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolox.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
